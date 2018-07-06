ee56abbf93ab57ef2fd1639a2ae009e1a2e6c8ae
Nate Miller <natekmiller96@gmail.com>
2018-07-06 20:06:01 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,12 @@
-# require libraries/modules here
+require 'nokogiri'
+require 'pry'
+html = File.read('fixtures/kickstarter.html')
+ 
+kickstarter = Nokogiri::HTML(html)
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+ 
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
